0c598de67ddf79e604ee41505dfe16c033633fc6
xles <xles@mirakulix.org>
2014-01-26 01:52:40 +0100

    Updated styles a bit...

--- footer.php
+++ footer.php
@@ -1,11 +1,32 @@
-<div class="clear"></div>
-</div>
-<footer id="footer" role="contentinfo">
-<div id="copyright">
+
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr>
+      <div class="row">
+        <div class="large-6 columns">
 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'draya' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'draya' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
-</div>
-</footer>
-</div>
+        </div>
+        <div class="large-6 columns">
+          <ul class="inline-list right">
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+            <li><a href="#">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 <?php wp_footer(); ?>
+    <script src="//cdn.jsdelivr.net/foundation/5.0.2/js/jquery.js"></script>
+    <!--
+    <script src="//cdn.jsdelivr.net/foundation/5.0.2/js/foundation.min.js"></script>
+    -->
+    <script src="//cdn.jsdelivr.net/foundation/5.0.2/js/foundation/foundation.js"></script>
+    <script src="//cdn.jsdelivr.net/foundation/5.0.2/js/foundation/foundation.topbar.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  
 </body>
 </html>
