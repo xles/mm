f97a178e43239694732ea3c31d5494f454aacb5a
xles <xles@mirakulix.org>
2014-01-28 01:55:54 +0100

    Added custom search form.

---
+++ searchform.php
@@ -0,0 +1,18 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<li class="has-form">
+	  <div class="row collapse">
+	    <div class="large-8 small-9 columns">
+	      <input type="text" name="s" placeholder="Find Stuff">
+	    </div>
+	    <div class="large-4 small-3 columns">
+				<input type="submit" class="alert button expand" value="Search">
+	    </div>
+	  </div>
+	</li>
+</form>
+<!--
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search …" value="" title="Search for:" />
+	</label>
+-->
