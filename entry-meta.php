b6a4a88ca42a4de2fe11dd238d4168c1b4bfe2c2
xles <xles@mirakulix.org>
2014-02-02 23:59:08 +0100

    Altered entrymeta.

--- entry-meta.php
+++ entry-meta.php
@@ -1,6 +1,7 @@
 <?php if( get_theme_mod( 'post_display_username' ) ) { ?>
+<!--
 	<span class="author vcard"><?php the_author_posts_link(); ?></span>
+-->
+	<?php the_author_posts_link(); ?> | 
 <?php } ?> 
-
-<span class="meta-sep"> | </span>
 <time datetime="<?php the_time("c"); ?>"><?php the_time( get_option( 'date_format' ) ); ?></span>
