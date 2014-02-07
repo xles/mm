77e6bae47333df7d97f320d663ce07f5d3050d61
xles <xles@mirakulix.org>
2014-02-07 23:57:36 +0100

    Changed superfluous section to main.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+<main id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
 <?php endwhile; endif; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
-</section>
+</main>
 </div>
 <div class="large-3 columns">
 <?php get_sidebar(); ?>
