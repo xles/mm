4c1127badaafa891e36136062ee6f242c59fdda0
xles <xles@mirakulix.org>
2014-02-19 14:52:57 +0100

    Made the read more button bigger.

--- entry-content.php
+++ entry-content.php
@@ -1,3 +1,3 @@
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content('<span class="tiny button right">Read more <i class="fi-arrow-right"></i></span>'); ?>
+<?php the_content('<span class="radius button right">Read more <i class="fi-arrow-right"></i></span>'); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
