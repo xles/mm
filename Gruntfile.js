0fb84bab50d2a844ae189dc75b453092f8be388a
xles <xles@mirakulix.org>
2014-02-21 10:59:24 +0100

    Added gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,34 @@
-{}
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: [
+				'Gruntfile.js'
+			]
+		},
+		cssmin: {
+			css:{
+				src: 'css/draya.css',
+				dest: 'css/draya.min.css'
+			}
+		},
+		uglify: {
+			js: {
+				files: {
+					'js/Hyphenator.min.js': [
+						'js/Hyphenator.js'
+					]
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'cssmin', 'uglify']);
+
+};
