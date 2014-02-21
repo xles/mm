eaa4ef4f73dcd8f3197b615b291aa46d056ae803
xles <xles@mirakulix.org>
2014-02-21 18:10:19 +0100

    Rearranged the project directory structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,17 +8,58 @@ module.exports = function(grunt) {
 		},
 		cssmin: {
 			css:{
-				src: 'css/draya.css',
-				dest: 'css/draya.min.css'
+				src: 'src/css/draya.css',
+				dest: 'dist/css/draya.min.css'
 			}
 		},
 		uglify: {
 			js: {
 				files: {
-					'js/Hyphenator.min.js': [
-						'js/Hyphenator.js'
+					'dist/js/Hyphenator.min.js': [
+						'src/js/Hyphenator.js'
 					]
 				}
+			}
+		},
+		copy: {
+			dist: {
+				files: [
+					{
+						expand: true, 
+						cwd: 'src/css/',
+						src: [
+							'foundation-icons/**',
+							'webicons-master/**',
+						], 
+						dest: 'dist/css/'
+					},
+					{
+						expand: true, 
+						cwd: 'src/js/',
+						src: [
+							'patterns/**'
+						], 
+						dest: 'dist/js/'
+					},
+					{
+						expand: true, 
+						cwd: 'src/img/',
+						src: ['**'], 
+						dest: 'dist/img/'
+					},
+					{
+						expand: true, 
+						cwd: 'src/',
+						src: ['*.php'], 
+						dest: 'dist/'
+					},
+					{
+						expand: true, 
+						cwd: 'src/meta/',
+						src: ['**'], 
+						dest: 'dist/'
+					}
+				]
 			}
 		}
 	});
@@ -27,8 +68,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint', 'cssmin', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'cssmin', 'uglify', 'copy']);
 
 };
